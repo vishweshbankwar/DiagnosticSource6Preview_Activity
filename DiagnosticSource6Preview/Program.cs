d6f675d7c3edc5f4fce519bab31aac852c75abf4
Vishwesh Bankwar <vibankwa@microsoft.com>
2021-05-26 20:07:53 -0700

    Diagnostic6preview example

---
+++ DiagnosticSource6Preview/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DiagnosticSource6Preview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new ActivityListener
+            {
+                ShouldListenTo = _ => true,
+                Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllData,
+            };
+
+            ActivitySource.AddActivityListener(listener);
+
+            ActivitySource activitySource = new ActivitySource("TestActivitySource");
+            
+            Activity.ForceDefaultIdFormat = true;
+            using (var act = activitySource.StartActivity("TestActivity1", ActivityKind.Client, "InvalidW3CIdParent"))
+            {
+                // act will not be null.
+                Console.WriteLine($"Is Activity created with ForceDefaultIdFormat set to True: {act!=null}");
+            }
+
+            Activity.ForceDefaultIdFormat = false;
+            using (var act = activitySource.StartActivity("TestActivity2", ActivityKind.Client, "InvalidW3CIdParent"))
+            {
+                // act will be null
+                Console.WriteLine($"Is Activity created with ForceDefaultIdFormat set to False: {act!=null}");
+            }
+        }
+    }
+}
